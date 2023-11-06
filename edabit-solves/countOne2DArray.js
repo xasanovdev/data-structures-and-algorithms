c9810aeb136115e7cd87fae375e09b8ff6663d57
Xasanov Ibrohim <you@example.com>
2023-11-06 21:06:53 +0500

    countOnes problem solved

---
+++ edabit-solves/countOne2DArray.js
@@ -0,0 +1,11 @@
+function countOnes(matrix) {
+  let summ = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == 1) summ++;
+    }
+  }
+
+  return summ;
+}
